43b1d84241ccd89a2c8ac3734f5dfcd12dba5b00
JackpotMachine777 <jackpotmachinedev777@gmail.com>
2026-06-13 03:49:05 +0200

    Added working main.rs example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,53 @@
-// use rstorch::tensor::Tensor;
-// use rstorch::ops::matmul::matmul;
+use rstorch::tensor::Tensor;
+use rstorch::nn::module::Module;
+use rstorch::nn::linear::Linear;
+use rstorch::nn::sequential::Sequential;
+use rstorch::ops::elementwise::mse_node::mse_node;
+use rstorch::optim::sgd::SGD;
+use rstorch::nn::activations::relu::ReLU;
+use rstorch::autograd::node::Node;
+use rstorch::init::he;
 
-fn main() {
-    todo!()
+fn main(){
+    let mut model = Sequential{
+        list: vec![
+            Box::new(Linear{
+                weights: Tensor::new(he::he(2, 2), vec![2, 2]),
+                bias: Tensor::new(vec![0.0, 0.0], vec![2]),
+                weights_node: None,
+                bias_node: None,
+            }),
+            Box::new(ReLU {}),
+            Box::new(Linear{
+                weights: Tensor::new(he::he(2, 2), vec![2, 2]),
+                bias: Tensor::new(vec![0.0, 0.0], vec![2]),
+                weights_node: None,
+                bias_node: None,
+            }),
+        ],
+    };
+
+    let input = Node::new(vec![1.0, 2.0], vec![1, 2]);
+    let target = Node::new(vec![0.0, 1.0], vec![1, 2]);
+    let mut sgd = SGD { lr: 0.001, momentum: 0.5, velocity: vec![] };
+
+    for epoch in 0..100 {
+        model.zero_grad();
+        let output = model.forward(input.clone());
+        let loss = mse_node(output.clone(), target.clone());
+
+        let grad_data: Vec<f32> = {
+            let p = output.borrow();
+            let t = target.borrow();
+            let n = p.data.len();
+            (0..n).map(|i| 2.0 * (p.data[i] - t.data[i]) / n as f32).collect()
+        };
+        output.borrow_mut().grad = grad_data;
+        output.borrow_mut().backward();
+
+        model.sync_grads();
+        sgd.step(&mut model.list);
+
+        println!("Epoch {}: loss = {}", epoch, loss);
+    }
 }
