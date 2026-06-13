1f9c18602363092d74ae6717cc6a8f68231e8a9c
JackpotMachine777 <jackpotmachinedev777@gmail.com>
2026-06-13 02:05:59 +0200

    Added autograd matmul test, fixed mod.rs in graph directory

--- src/autograd/graph/mod.rs
+++ src/autograd/graph/mod.rs
@@ -4,3 +4,10 @@ pub mod mul;
 pub mod relu;
 pub mod sigmoid;
 pub mod tanh;
+
+pub use add::add;
+pub use mul::mul;
+pub use relu::relu;
+pub use sigmoid::sigmoid;
+pub use tanh::tanh;
+pub use matmul::matmul;
