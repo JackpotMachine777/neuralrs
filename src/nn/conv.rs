52a9362a6efe3bdb861531684ddbe6d48bdf6b32
JackpotMachine777 <jackpotmachinedev777@gmail.com>
2026-06-14 04:33:07 +0200

    Add multichannel Conv2d forward test

---
+++ src/nn/conv.rs
@@ -0,0 +1,81 @@
+use crate::tensor::Tensor;
+use crate::nn::module::Module;
+use crate::autograd::node::Node;
+use std::rc::Rc;
+use std::cell::RefCell;
+
+pub struct Conv2d {
+    pub weight: Tensor,
+    pub bias: Tensor,
+    pub c_in: usize,
+    pub c_out: usize,
+    pub kh: usize,
+    pub kw: usize,
+    pub stride: usize,
+    pub in_h: usize,
+    pub in_w: usize,
+    pub weight_grad: Rc<RefCell<Vec<f32>>>,
+    pub bias_grad: Rc<RefCell<Vec<f32>>>,
+}
+
+impl Module for Conv2d {
+    fn forward(&mut self, input: Rc<RefCell<Node>>) -> Rc<RefCell<Node>> {
+        let data = input.borrow().data.clone();
+
+        let c_in = self.c_in;
+        let c_out = self.c_out;
+        let kh = self.kh;
+        let kw = self.kw;
+        let stride = self.stride;
+        let in_h = self.in_h;
+        let in_w = self.in_w;
+
+        let out_h = (in_h - kh) / stride + 1;
+        let out_w = (in_w - kw) / stride + 1;
+
+        let weight = self.weight.storage.data.clone();
+        let bias = self.bias.storage.data.clone();
+
+        let mut out = vec![0.0; c_out * out_h * out_w];
+
+        for oc in 0..c_out {
+            for oy in 0..out_h {
+                for ox in 0..out_w {
+                    let mut sum = 0.0;
+
+                    for ic in 0..c_in {
+                        for i in 0..kh {
+                            for j in 0..kw {
+                                let iy = oy * stride + i;
+                                let ix = ox * stride + j;
+
+                                let in_idx = (ic * in_h + iy) * in_w + ix;
+                                let w_idx = ((oc * c_in + ic) * kh + i) * kw + j;
+
+                                sum += data[in_idx] * weight[w_idx];
+                            }
+                        }
+                    }
+
+                    sum += bias[oc];
+
+                    let out_idx = (oc * out_h + oy) * out_w + ox;
+                    out[out_idx] = sum;
+                }
+            }
+        }
+
+        let shape = vec![c_out, out_h, out_w];
+
+        Node::new(out, shape)
+    }
+
+    fn parameters(&mut self) -> Vec<&mut Tensor> {
+        vec![&mut self.weight, &mut self.bias]
+    }
+
+    fn zero_grad(&mut self) {
+        self.weight.grad = vec![0.0; self.weight.storage.data.len()];
+        self.bias.grad = vec![0.0; self.bias.storage.data.len()];
+    }
+}
---
+++ tests/nn_conv_test.rs
@@ -0,0 +1,68 @@
+use rstorch::tensor::Tensor;
+use rstorch::nn::module::Module;
+use rstorch::nn::conv::Conv2d;
+use rstorch::autograd::node::Node;
+
+#[test]
+fn conv2d_forward_test() {
+    let mut layer = Conv2d {
+        weight: Tensor::new(vec![1.0, 1.0, 1.0, 1.0], vec![1, 1, 2, 2]),
+        bias: Tensor::new(vec![0.0], vec![1]),
+        c_in: 1,
+        c_out: 1,
+        kh: 2,
+        kw: 2,
+        stride: 1,
+        in_h: 3,
+        in_w: 3,
+        weight_grad: std::rc::Rc::new(std::cell::RefCell::new(vec![0.0; 4])),
+        bias_grad: std::rc::Rc::new(std::cell::RefCell::new(vec![0.0; 1])),
+    };
+
+    let input = Node::new(vec![
+        1.0, 2.0, 3.0,
+        4.0, 5.0, 6.0,
+        7.0, 8.0, 9.0,
+    ], vec![1, 3, 3]);
+
+    let output = layer.forward(input);
+
+    println!("output: {:?}", output.borrow().data);
+    println!("shape:  {:?}", output.borrow().shape);
+
+    assert_eq!(output.borrow().shape, vec![1, 2, 2]);
+    assert_eq!(output.borrow().data, vec![12.0, 16.0, 24.0, 28.0]);
+}
+
+#[test]
+fn conv2d_multichannel_test() {
+    let mut layer = Conv2d {
+        weight: Tensor::new(vec![
+            1.0, 1.0, 1.0, 1.0,  1.0, 1.0, 1.0, 1.0,
+            2.0, 2.0, 2.0, 2.0,  2.0, 2.0, 2.0, 2.0,
+        ], vec![2, 2, 2, 2]),
+        bias: Tensor::new(vec![0.0, 0.0], vec![2]),
+        c_in: 2,
+        c_out: 2,
+        kh: 2,
+        kw: 2,
+        stride: 1,
+        in_h: 2,
+        in_w: 2,
+        weight_grad: std::rc::Rc::new(std::cell::RefCell::new(vec![0.0; 16])),
+        bias_grad: std::rc::Rc::new(std::cell::RefCell::new(vec![0.0; 2])),
+    };
+
+    let input = Node::new(vec![
+        1.0, 2.0, 3.0, 4.0,
+        5.0, 6.0, 7.0, 8.0, 
+    ], vec![2, 2, 2]);
+
+    let output = layer.forward(input);
+
+    println!("output: {:?}", output.borrow().data);
+    println!("shape:  {:?}", output.borrow().shape);
+
+    assert_eq!(output.borrow().shape, vec![2, 1, 1]);
+    assert_eq!(output.borrow().data, vec![36.0, 72.0]);
+}
