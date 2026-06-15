801565c9e837fea328c853674c9904649601ea39
JackpotMachine777 <jackpotmachinedev777@gmail.com>
2026-06-15 23:45:37 +0200

    Added batch support to Linear (broadcast bias in graph add)

--- src/autograd/graph/add.rs
+++ src/autograd/graph/add.rs
@@ -2,13 +2,29 @@ use std::{rc::Rc, cell::RefCell};
 use crate::autograd::node::Node;
 
 pub fn add(a: Rc<RefCell<Node>>, b: Rc<RefCell<Node>>) -> Rc<RefCell<Node>> {
-    let data: Vec<f32> = a.borrow().data.iter()
-        .zip(b.borrow().data.iter())
-        .map(|(x, y)| x + y)
-        .collect();
-    
-    let shape = a.borrow().shape.clone();
-    
+    let a_data = a.borrow().data.clone(); 
+    let a_shape = a.borrow().shape.clone();
+    let b_data = b.borrow().data.clone();
+    let b_shape = b.borrow().shape.clone();
+
+    let broadcast = a_shape.len() == 2 && b_shape.len() == 1 && a_shape[1] == b_shape[0];
+
+    let data: Vec<f32> = if broadcast {
+        let batch = a_shape[0];
+        let features = a_shape[1];
+        let mut out = vec![0.0; a_data.len()];
+        for bi in 0..batch {
+            for f in 0..features {
+                let idx = bi * features + f;
+                out[idx] = a_data[idx] + b_data[f];
+            }
+        }
+        out
+    } else {
+        a_data.iter().zip(b_data.iter()).map(|(x, y)| x + y).collect()
+    };
+
+    let shape = a_shape.clone();
     let n = Node::new(data, shape);
 
     {
@@ -18,11 +34,30 @@ pub fn add(a: Rc<RefCell<Node>>, b: Rc<RefCell<Node>>) -> Rc<RefCell<Node>> {
 
     let a_clone = a.clone();
     let b_clone = b.clone();
+    let a_shape_c = a_shape.clone();
+    let b_shape_c = b_shape.clone();
 
     n.borrow_mut().backward_fn = Some(Box::new(move |grad: &Vec<f32>| {
-        for i in 0..grad.len() {
-            a_clone.borrow_mut().grad[i] += grad[i];
-            b_clone.borrow_mut().grad[i] += grad[i];
+        let broadcast = a_shape_c.len() == 2 && b_shape_c.len() == 1 && a_shape_c[1] == b_shape_c[0];
+
+        if broadcast {
+            let batch = a_shape_c[0];
+            let features = a_shape_c[1];
+
+            for i in 0..grad.len() {
+                a_clone.borrow_mut().grad[i] += grad[i];
+            }
+
+            for bi in 0..batch {
+                for f in 0..features {
+                    b_clone.borrow_mut().grad[f] += grad[bi * features + f];
+                }
+            }
+        } else {
+            for i in 0..grad.len() {
+                a_clone.borrow_mut().grad[i] += grad[i];
+                b_clone.borrow_mut().grad[i] += grad[i];
+            }
         }
     }));
 
---
+++ tests/nn_linear_batch_test.rs
@@ -0,0 +1,40 @@
+use rstorch::tensor::Tensor;
+use rstorch::nn::module::Module;
+use rstorch::nn::linear::Linear;
+use rstorch::autograd::node::Node;
+
+#[test]
+fn linear_batch_test() {
+    let mut layer = Linear {
+        weights: Tensor::new(vec![
+            1.0, 0.0,
+            0.0, 1.0,
+            1.0, 1.0,
+        ], vec![3, 2]),
+        bias: Tensor::new(vec![10.0, 20.0], vec![2]),
+        weights_node: None,
+        bias_node: None,
+    };
+
+    let input = Node::new(vec![
+        1.0, 2.0, 3.0,
+        4.0, 5.0, 6.0,
+    ], vec![2, 3]);
+
+    let output = layer.forward(input.clone());
+
+    println!("output: {:?}", output.borrow().data);
+    println!("shape:  {:?}", output.borrow().shape);
+
+    assert_eq!(output.borrow().shape, vec![2, 2]);
+    assert_eq!(output.borrow().data, vec![14.0, 25.0, 20.0, 31.0]);
+
+    output.borrow_mut().grad = vec![1.0, 1.0, 1.0, 1.0];
+    output.borrow_mut().backward();
+
+    layer.sync_grads();
+    println!("bias grad: {:?}", layer.bias.grad);
+    assert_eq!(layer.bias.grad, vec![2.0, 2.0]);
+
+    println!("linear batch ok");
+}
